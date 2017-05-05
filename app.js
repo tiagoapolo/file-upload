a57c8feb6e3c1c3005396d533ffd6f06dd85952f
Jean Carlo Nascimento <jnascimento@gmail.com>
2017-05-05 17:19:28 -0300

    Naoo testei mas deve fungar LOL

--- app.js
+++ app.js
@@ -1,65 +1,63 @@
-var express = require('express');
-var fileUpload = require('express-fileupload');
-var app = express();
-var bodyParser= require('body-parser');
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const app = express()
+const bodyParser= require('body-parser')
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000)
 // default options
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Headers', 'my-header,X-Requested-With,content-type,Authorization,cache-control');
-  next();
-});
-app.use(bodyParser.json());
+app.use( (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader('Access-Control-Allow-Headers', 'my-header,X-Requested-With,content-type,Authorization,cache-control')
+  next()
+})
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
-}));
-app.use(fileUpload());
+}))
+app.use(fileUpload())
 app.use(express.static(__dirname + "/public"))
 
-var router = express.Router();
+const router = express.Router()
 
-router.post('/upload', function(req, res) {
+const error = ( err, res ) => {
+  console.log( err )
+  res.status(500).send('Error')
+}
 
-  var now = Date.now();
-  var sampleFile;
-  var email;
+router.post('/upload', (req, res) => {
 
-
-  // console.log(req.files);
+// Funcao sempre deve retornar algo
+  // console.log(req.files)
   if (!req.files && !req.body.email) {
-    res.send('No files were uploaded.');
-    return;
+    res.send('No files were uploaded.')
+    return false
   }
 
-
-  if((req.files.sampleFile.mimetype != 'application/pdf') && (req.files.sampleFile.mimetype != 'image/png') && (req.files.sampleFile.mimetype != 'image/jpeg')){
-    res.send('File format not accepted');
-    return;
+  if( (req.files.sampleFile.mimetype != 'application/pdf') && 
+      (req.files.sampleFile.mimetype != 'image/png') && 
+      (req.files.sampleFile.mimetype != 'image/jpeg') ){
+    res.send('File format not accepted')
+    return false
   }
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  sampleFile = req.files.sampleFile;
-  var fileNameType = sampleFile.name;
-  var fileType = fileNameType.substring(fileNameType.indexOf('.'),fileNameType.length);
-  email = req.body.email;
+  const sampleFile = req.files.sampleFile
+  const fileNameType = sampleFile.name
+  const fileType = fileNameType.substring( fileNameType.indexOf('.'), fileNameType.length)
+  const email = req.body.email
 
-  sampleFile.name = email.substring(0,email.indexOf('@')) + '-' + now + fileType;
+  sampleFile.name = email.substring( 0, email.indexOf('@') ) + 
+                                    `- ${Date.now()}${fileType}`
 
   // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv('./public/' + sampleFile.name, function(err) {
-      if (err) {
-        console.log(err);
-        res.status(500).send('Error');
-      }
-      else {
-        res.send('File uploaded!');
-      }
-    });
-  });
+  sampleFile.mv('./public/' + sampleFile.name,  (err) => 
+    ( err ) 
+      ? error( err, res )
+      : res.send('File uploaded!'))
+  })
 
-app.use('/contax', router);
+app.use( '/contax', router )
 
-var server = app.listen(app.get('port'), function () {
-                console.log('Server running at port: ' + server.address().port);
-             });
+const server = app.listen( app.get( 'port' ), () => {
+    console.log( 'Server running at port: ' + server.address().port )
+ })
